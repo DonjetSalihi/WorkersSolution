b6bbc17b318ef5103579e7fa1bfba168d5f50470
Donjet Salihi <dsalihi@mobilum.com>
2023-09-15 11:12:15 +0200

    Made some changes

--- WorkersSolution/WorkersSolution/Constants/AppConfig.cs
+++ WorkersSolution/WorkersSolution/Constants/AppConfig.cs
@@ -2,10 +2,10 @@
 {
     public static class AppConfig
     {
-        public static string WorkersApiBaseUrl = "https://dev-api-1.sitedocs.com/api/v1/";
+        public static string WorkersApiBaseUrl = ""; //Please add the base url
 
         //We can store the authorization token in secure storage from xamarin essentials,
         //but since in this project we are not retrieving it from login or somewhere, I didn't store it.
-        public static string WorkersApiAuthorizationToken = "Token 44798935-c223-47e6-b0eb-84df6c6210c7";
+        public static string WorkersApiAuthorizationToken = ""; // Please add the authorization token value
     }
 }
